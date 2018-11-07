80f9b931dd73f521a084de5b257594fc4336a41e
eko <eko@whitestein.com>
2018-11-07 09:59:24 +0100

    init

---
+++ src/main/kotlin/forms/PhpFileAssembler.kt
@@ -0,0 +1,63 @@
+package forms
+
+import java.io.File
+import java.io.InputStream
+import java.nio.charset.Charset
+
+class PhpFileAssembler() {
+
+
+  public val indexFile: String = "/home/eko/repos/lezecka-akademia/target/sk/about/index.html";
+
+  fun createPhpFile(contentFile: String, outputFile: String) {
+
+    var header: String;
+    var footer: String;
+    var form: String;
+
+    var indexContent: String;
+
+    form = contentFile;
+
+    indexContent = getIndexContent(indexFile);
+
+    header = getHeader(indexContent);
+    footer = getFooter(indexContent);
+    form = header + getBody(contentFile) + footer;
+
+
+    createFormFile(form, outputFile);
+  }
+
+  fun getBody(bodyFile: String): String {
+    val file = File(bodyFile);
+    var ins: InputStream = file.inputStream();
+    var content = ins.readBytes().toString(Charset.defaultCharset());
+    return content;
+  }
+
+  fun getFooter(content: String): String {
+    val footerSplit = content.split("<footer class=\"footer\">".toRegex());
+    val footer = footerSplit.last();
+    return "<footer class=\"footer\">" + footer
+  }
+
+  fun getHeader(content: String): String {
+    val headerSplit = content.split("</nav>".toRegex());
+    return "<?php\nrequire('validation.php')\n?>" + headerSplit[0] + "</nav>\n";
+  }
+
+  fun getIndexContent(indexFile: String): String {
+    val file = File(indexFile);
+    var ins: InputStream = file.inputStream();
+    var content = ins.readBytes().toString(Charset.defaultCharset());
+    content = content.replace("\n", "");
+    return content;
+  }
+
+  fun createFormFile(content: String, target: String) {
+
+    var file = File(target);
+    file.writeText(content);
+  }
+}
