723204f6c7adcc4232c89872fbb4c888938d61d9
eko <eko@whitestein.com>
2018-11-08 09:42:46 +0100

    build forms as part of build

--- src/main/kotlin/forms/FormBuilderFun.kt
+++ src/main/kotlin/forms/FormBuilderFun.kt
@@ -1,28 +1,19 @@
 package forms
 
-import java.io.BufferedReader
-import java.io.File
-import java.io.InputStream
-import java.nio.charset.Charset
-import java.util.regex.Matcher
-import java.util.regex.Pattern
-
 fun main(args : Array<String>){
-  val root = "/home/eko/repos/lezecka-akademia/src/main/resources/";
-  val localRoot = "/home/eko/repos/lezecka-akademia-gradle/"
-  val src = root + "static/forms/simple/";
-  val target = localRoot + "target/forms/simple/";
+  val root = "/home/eko/repos/lezecka-akademia/"
+  val src = root + "src/main/resources/resources/forms/simple/"
+  val target = root + "target/forms/simple/"
   
-  
-  val filesSrcAndTarget = HashMap<String, String>();
-  //list of php files to generate:
-  val files = arrayOf("simple_application.php", "zoznam.php", "submit_confirmation.php");
-  
+    //list of php files to generate:
+  val files = arrayOf("simple_application.php", "zoznam.php", "submit_confirmation.php")
+  val filesSrcAndTarget = HashMap<String, String>()
+
   //create map of source path and target path for each file:
     files.forEach {
-      filesSrcAndTarget[src+it] = target+it;
-  }
-  
+      filesSrcAndTarget[src + it] = target + it
+    }
+
   //create the file from the source path in the target path:
   filesSrcAndTarget.forEach {
     k,v -> PhpFileAssembler().createPhpFile(k,v)
--- src/main/kotlin/forms/PhpFileAssembler.kt
+++ src/main/kotlin/forms/PhpFileAssembler.kt
@@ -4,60 +4,55 @@ import java.io.File
 import java.io.InputStream
 import java.nio.charset.Charset
 
-class PhpFileAssembler() {
+class PhpFileAssembler {
 
 
-  public val indexFile: String = "/home/eko/repos/lezecka-akademia/target/sk/about/index.html";
+  private val indexFile: String = "/home/eko/repos/lezecka-akademia/target/sk/about/index.html"
 
   fun createPhpFile(contentFile: String, outputFile: String) {
 
-    var header: String;
-    var footer: String;
-    var form: String;
+    var header: String
+    var footer: String
+    var form: String
 
-    var indexContent: String;
+    var indexContent: String = getIndexContent(indexFile)
 
-    form = contentFile;
-
-    indexContent = getIndexContent(indexFile);
-
-    header = getHeader(indexContent);
-    footer = getFooter(indexContent);
-    form = header + getBody(contentFile) + footer;
+    header = getHeader(indexContent)
+    footer = getFooter(indexContent)
+    form = header + getBody(contentFile) + footer
 
 
-    createFormFile(form, outputFile);
+    createFormFile(form, outputFile)
   }
 
   fun getBody(bodyFile: String): String {
-    val file = File(bodyFile);
-    var ins: InputStream = file.inputStream();
-    var content = ins.readBytes().toString(Charset.defaultCharset());
-    return content;
+    val file = File(bodyFile)
+    var ins: InputStream = file.inputStream()
+    return ins.readBytes().toString(Charset.defaultCharset())
   }
 
   fun getFooter(content: String): String {
-    val footerSplit = content.split("<footer class=\"footer\">".toRegex());
-    val footer = footerSplit.last();
+    val footerSplit = content.split("<footer class=\"footer\">".toRegex())
+    val footer = footerSplit.last()
     return "<footer class=\"footer\">" + footer
   }
 
   fun getHeader(content: String): String {
-    val headerSplit = content.split("</nav>".toRegex());
-    return "<?php\nrequire('validation.php')\n?>" + headerSplit[0] + "</nav>\n";
+    val headerSplit = content.split("</nav>".toRegex())
+    return "<?php\nrequire('validation.php')\n?>" + headerSplit[0] + "</nav>\n"
   }
 
   fun getIndexContent(indexFile: String): String {
-    val file = File(indexFile);
-    var ins: InputStream = file.inputStream();
-    var content = ins.readBytes().toString(Charset.defaultCharset());
-    content = content.replace("\n", "");
-    return content;
+    val file = File(indexFile)
+    var ins: InputStream = file.inputStream()
+    var content = ins.readBytes().toString(Charset.defaultCharset())
+    content = content.replace("\n", "")
+    return content
   }
 
   fun createFormFile(content: String, target: String) {
 
-    var file = File(target);
-    file.writeText(content);
+    var file = File(target)
+    file.writeText(content)
   }
 }
